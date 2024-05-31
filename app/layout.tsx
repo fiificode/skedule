b13d2c047112364872e4fdb9488ad83d729127f9
franklin mensah <fiificode@gmail.com>
2024-05-31 12:46:35 +0000

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Skedule",
+  description:
+    "Skedule is an appointment/meeting application to help schedule appointments or meetings for professionals.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
